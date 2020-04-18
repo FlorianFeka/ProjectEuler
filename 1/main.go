b70ad32cf18cee4ce0c3e66b05561ffae62f2d76
FlorianFeka <florianfeka05@gmail.com>
2020-04-18 18:45:07 +0200

    add euler solution to problem 1

--- 1/main.go
+++ 1/main.go
@@ -2,12 +2,24 @@ package main
 
 import "fmt"
 
+var target = 999
+
 func main() {
+	// my simple solution
 	sum := 0
-	for i := 1; i < 1000; i++ {
+	for i := 1; i < 1_000; i++ {
 		if i%3 == 0 || i%5 == 0 {
 			sum += i
 		}
 	}
 	fmt.Println(sum)
+
+	// Euler Project solution (https://projecteuler.net/overview=001)
+	res := SumDivisibleBy(3) + SumDivisibleBy(5) - SumDivisibleBy(15)
+	fmt.Println(res)
+}
+
+func SumDivisibleBy(n int) int {
+	p := target / n
+	return n * (p * (p + 1)) / 2
 }
